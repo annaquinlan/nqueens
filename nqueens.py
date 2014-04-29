f19241aa50d51be68d90dbaaf104e07878017ba6
quinlana <quinlana@cmc306-17.mathcs.carleton.edu>
2014-04-28 19:44:50 -0500

    working with comments

--- nqueens.py
+++ nqueens.py
@@ -1,25 +1,31 @@
+# N-Queens Problem
 # Written by Sophia Davis and Anna Quinlan, 4/27/14
-import random
+
 import sets
 
-# Returns solution to for n by n board.
+# Returns n-queens solution for n by n board.
 def nqueens(n):
     if n <= 3:
         return false
-    # make the board
+    # Make the board as a list of rows.
     board = []
     j = 0
     for i in range(0, n):
         board.append(range(j, j + n))
         j += n
+    
+    # Assignment list contains queen locations. Options contains lists of possible assignments
+    # given already assigned queen locations.
+    # i.e. Options[1] is remaining "safe" locations after placing one queen.
     assignment = []
-    possible = [range(0, n*n)]
-    return backtrack(assignment, possible, n, board)
+    options = [range(0, n*n)]
+    return backtrack(assignment, options, n, board)
 
-# with '0' as [0,0]
+# Returns the (x,y) coordinates of a number on the board with '0' as [0,0]
 def getindex(i, n):
     return (i%n, i/n)
 
+# Given a number on the board, these functions return numbers in the same row, column, or diagonals.
 def getrow(board, i, n):
     return board[i / n]
 
@@ -67,42 +73,50 @@ def getdiagsright(board, i, n):
         y += 1
     return diags
 
+# Prints the board
 def printboard(board, n):
     for i in range(0, n):
         print board[i]
-        
-def backtrack(assignment, possible, n, board):
+
+# Implementation of backtracking search        
+def backtrack(assignment, options, n, board):
     if len(assignment) == n:
         return assignment
-#     print "Options are"
-    currentoptions = possible[-1]
-#     print possible
-#     print currentoptions
-    # get variable to assign
+    
+    # Given last queen assignment, the last list in 'options' contains the remaining "safe" locations
+    # to potentially put next queen. 
+    currentoptions = options[-1]
+    
+    # Get a "safe" location
     for loc in currentoptions:
-        # if value is consistent with assignment
-            # add {var = value} to assignment
+        
+        # Add location to assignment
         assignment.append(loc)
-            # perform inference
-        restricted = inference(board, loc, n, currentoptions)
-        # if inference  not a failure
-        if len(restricted) >= (n - len(assignment)):
-            possible.append(restricted)
-            result = backtrack(assignment, possible, n, board)
+        
+        # Perform inference
+        safe = inference(board, loc, n, currentoptions)
+        
+        # If inference not a failure (there are still more "safe" locations than queens we need to place)
+        if len(safe) >= (n - len(assignment)):
+            options.append(safe)
+            result = backtrack(assignment, options, n, board)
             if result:
                 return assignment
-        # remove {var = value} and inferences from assignment
+        
+        # Get rid of last queen assignment if can't go further
         assignment.pop()
     return False
     
-# return all locations that can still have a queen    
-def inference(board, i, n, possible):
-    possibleset = sets.Set(possible)
+# Return all "safe" locations that can still have a queen  
+# We used sets to filter locations that can no longer have a queen
+def inference(board, i, n, options):
+    optionsset = sets.Set(options)
     rows = sets.Set(getrow(board, i, n))
     cols = sets.Set(getcol(board, i, n))
     ldiags = sets.Set(getdiagsleft(board, i, n))
     rdiags = sets.Set(getdiagsright(board, i, n))
-    updated = possibleset.difference(rows).difference(cols).difference(ldiags).difference(rdiags)
+    updated = optionsset.difference(rows).difference(cols).difference(ldiags).difference(rdiags)
     return list(updated)
 
-nqueens(8)
+# Test:
+# print nqueens(8)
